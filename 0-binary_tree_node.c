6fd8686f6d811562ebfc7a74f7ac4951cbc6e6ab
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2022-11-18 02:52:55 +0100

    updating remove and sort Solutions to Binary trees project tasks

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,23 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: is a pointer to the parent node of the node to create
- * @value: is the value to put in the new node
- * Return: a pointer to the new node, or NULL on failure
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = NULL;
+	binary_tree_t *new;
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
-	{
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
 		return (NULL);
-	}
-	node->parent = parent;
-	node->n = value;
-	node->left = NULL;
-	node->right = NULL;
-	return (node);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
 }
